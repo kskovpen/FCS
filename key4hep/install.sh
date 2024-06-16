b7be53d56470854da460ba77bcc95fb87c6ae757
Kirill Skovpen <kskovpen@cern.ch>
2024-06-16 08:47:27 +0200

    first commit

---
+++ key4hep/install.sh
@@ -0,0 +1,11 @@
+source spack/share/spack/setup-env.sh
+spack env activate key4hep-spack/environments/key4hep-release-user
+spack concretize -f
+spack find
+#source /cvmfs/sw.hsf.org/key4hep/setup.sh
+#. /HiggsAnalysis/CombinedLimit/env_lcg.sh
+#root-config --version
+#uname -a
+#gcc --version
+#cd HiggsAnalysis/CombinedLimit
+#/usr/bin/make LCG=1 -j 8
